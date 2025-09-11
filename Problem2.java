755eb16d5127509158d604a5d7a4e1667be48c50
cdnguyen125 <cdnguyen125@student.hbuhsd.edu>
2025-09-11 17:34:14 +0000

    finished

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,10 @@
-
+public class Problem2
+{
+  public static void main(String[] args)
+  {
+    double x = 123.45;
+    double y = 678.90;
+    int sum = (int)(x + 0.5) + (int)(y + 0.5);
+    System.out.printf("\nThe whole # sum of %3.2f and %3.2f is %d\n", x, y, sum);
+  }
+}
--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,23 @@
+public class Problem3
+{
+  public static void main(String[] args)
+  {
+    double x = 12345.67890;
+    double last3 = x*1000;
+    int intx = (int)last3; // 12345678
 
+    System.out.println("first three decimal digits of " + x + " are: " + ((intx/100) % 10) + " " + ((intx/10) % 10) + " " + (intx % 10));
+    // sol. 1 (amy's version) - less precise (w/o flags)
+
+    int lastDigit = intx % 10;
+    intx /= 10; 
+    int secondlastDigit = intx % 10;
+    intx /= 10;
+    int thirdlastDigit = intx % 10;
+
+    System.out.printf("\nfirst three decimal digits of %5.5f are %d %d %d\n", x, thirdlastDigit, secondlastDigit, lastDigit);
+    // sol. 2 (christian's version)
+
+
+  }
+}
